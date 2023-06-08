9462cd228a57ed8035f3cbb29af790ff2adeba22
Alvaro-PV <alvaropenalverval@gmail.com>
2023-06-08 21:09:27 +0100

    Added the layout & activity for the main menu and bare-bones functionality

---
+++ app/src/main/java/com/example/mazeapp/MainMenu/MainMenuActivity.java
@@ -0,0 +1,44 @@
+package com.example.mazeapp.MainMenu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mazeapp.App.Mediator;
+import com.example.mazeapp.MazeGenerator.MazeGeneratorActivity;
+import com.example.mazeapp.MazeSetup.MazeSetupActivity;
+import com.example.mazeapp.R;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    public static final String TAG = MazeGeneratorActivity.class.getSimpleName();
+
+    Button createMazeButtonView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate()");
+        super.onCreate(savedInstanceState);
+
+        try {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            getSupportActionBar().hide();
+        } catch (Exception e){}
+
+        setContentView(R.layout.main_menu_activity);
+
+        createMazeButtonView = findViewById(R.id.createMazeButtonView);
+        createMazeButtonView.setOnClickListener(view -> {
+            navigateToMazeSetupActivity();
+        });
+    }
+
+    private void navigateToMazeSetupActivity() {
+        Intent intent = new Intent(this, MazeSetupActivity.class);
+        startActivity(intent);
+    }
+}
